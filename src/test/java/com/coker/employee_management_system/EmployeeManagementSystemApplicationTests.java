dd8c3917bfeeafef40be27272dc0445911ff4f31
Tiana Coker <tianacoker1@gmail.com>
2024-06-20 19:42:45 +0100

    first commit

---
+++ src/test/java/com/coker/employee_management_system/EmployeeManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coker.employee_management_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
